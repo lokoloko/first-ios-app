f879cfeca44b3c9d4d85f47d8502830a3fa7ff77
richard <richard@macbook-air.local>
2015-09-23 20:41:35 -0700

    Initial Commit

---
+++ UnitConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UnitConverter
+//
+//  Created by richard on 9/23/15.
+//  Copyright © 2015 Richard Montoya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UnitConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UnitConverter
+//
+//  Created by richard on 9/23/15.
+//  Copyright © 2015 Richard Montoya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
